26423e6e9d1842cc737bcab2b17a6f0e83d1194d
abigraken <abigraken@gmail.com>
2021-10-28 22:41:17 -0400

    Colorblind compatibility added and documentation updated

--- env/lib/xsltConf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/home/abigail/Coding/cgb3/env/lib"
-XSLT_LIBS="-lxslt -L/home/abigail/Coding/cgb3/env/lib -lxml2 -L/home/abigail/Coding/cgb3/env/lib -L/home/abigail/Coding/cgb3/env/lib -lpthread -L/home/abigail/Coding/cgb3/env/lib -L/home/abigail/Coding/cgb3/env/lib -licui18n -licuuc -licudata -lm -ldl -lrt"
-XSLT_PRIVATE_LIBS="-lm"
-XSLT_INCLUDEDIR="-I/home/abigail/Coding/cgb3/env/include"
-MODULE_VERSION="xslt-1.1.34"
